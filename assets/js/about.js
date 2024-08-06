e229e16b0a0180e36c894f1adf66611d97dfda85
Kishan <kishanpradhan226@gmail.com>
2024-08-06 22:40:31 +0530

    added SVG and about page

---
+++ assets/js/about.js
@@ -0,0 +1,16 @@
+// Company location coordinates
+const companyLatitude = 37.7749;
+const companyLongitude = -122.4194;
+
+// Initialize the map
+var map = L.map('map').setView([companyLatitude, companyLongitude], 13);
+
+// Set up the OpenStreetMap layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Add a marker for the company location
+L.marker([companyLatitude, companyLongitude]).addTo(map)
+    .bindPopup('<b>TechThreads Headquarters</b><br>San Francisco, CA.')
+    .openPopup();
